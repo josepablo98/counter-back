7bcc7f8096f7d5cf3d37dd6af11c76e0ded90337
Jose Pablo <josepabloruizperez99@gmail.com>
2024-02-06 20:21:46 +0100

    cambios

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-// server.js
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
-
-const server = http.createServer(app);
-const io = socketIo(server, {
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
   cors: {
-    origin: "*", // Permite todas las origenes, cambia esto según tus necesidades
-    methods: ["GET", "POST"]
-  }
+    origin: '*',
+    methods: ['GET', 'POST']
+  },
+  transports: ['polling']
 });
-
-let counter = 0;
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
-  socket.emit('counter', counter);
-
-  socket.on('increment', () => {
-    counter++;
-    io.sockets.emit('counter', counter);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
+  console.log('a user connected');
 });
 
-server.listen(4001, () => console.log('Listening on port 4001'));
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server running on port ${port}`));
